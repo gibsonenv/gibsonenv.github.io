306f5bc0c7c61b47c1236b26d8f4b5f12878b391
Zhiyang He <zhiyanghe96@gmail.com>
2017-12-09 13:27:49 -0800

    Restructuring, added teemplate, easier to maintain

---
+++ index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var helpers = require('handlebars-helpers')();
+var app = express();
+var router = express.Router();
+
+app.set('port', (process.env.PORT || 5000))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+app.use(express.static(__dirname + '/public/'))
+app.use('/',router)
+app.enable('view cache');
+
+
+router.get('/',function(req, res){
+	res.render('index', {
+		title : "Home"
+	})
+});
+
+router.get('/models',function(req, res){
+    res.render('models', {
+		title: "Models"
+	});
+});
+
+router.get('/platform',function(req, res){
+	res.render('platform', {
+		title : "Platform"
+	});
+});
+
+router.get('/paper',function(req, res){
+  res.render('paper', {
+		title : "Paper"
+	});
+});
+
+
+app.listen(app.get('port'), function() {
+  console.log("Cambria app is running at localhost:" + app.get('port'))
+})
